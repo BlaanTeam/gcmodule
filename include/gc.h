202122769b7c863ebc611f8659ab62119daa1798
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2022-02-05 23:47:41 +0100

    fix: add  missing prototypes

--- include/gc.h
+++ include/gc.h
@@ -11,8 +11,8 @@
 #ifndef GC_H
 #define GC_H
 #include <stdlib.h>
-#define GC_CLR_ALL	(1 << 0)
-#define GC_CLR_TMP	(1 << 1)
+#define GC_ALL	1U
+#define GC_TMP	2U
 
 typedef struct s_dustbin
 {
@@ -20,13 +20,13 @@ typedef struct s_dustbin
 	struct s_dustbin	*next;
 }	t_dustbin;
 
-typedef unsigned short t_gc_flag;
+typedef unsigned short	t_gc_flag;
 typedef struct s_gc
 {
 	t_dustbin	*dustbin;
 	t_dustbin	*tmp_dustbin;
-	struct s_gc	*(*append)(struct s_gc *, void *);
-	void		(*clean)(struct s_gc *);
+	struct s_gc	*(*append)(struct s_gc *, void *, t_gc_flag);
+	void		(*clean)(struct s_gc **, t_gc_flag);
 }	t_gc;
 
 t_gc	*gc_init(void);
